907c1a664f84cb0e770a920aae57849bd7766783
Flajakay <33432218+Flajakay@users.noreply.github.com>
2023-03-21 17:03:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,23 +3,12 @@ include 'core/init.php';
 include 'template/overall_header.php';
 
 if(isset($_GET['page'])) {
-	$_GET['page'] = htmlspecialchars($_GET['page'], ENT_QUOTES);
-	$pages = glob("pages/" . "*.php");
-	$pages = preg_replace("(pages/|.php)", "", $pages);
-
-	if(in_array($_GET['page'], $pages)) {
-		include 'pages/'.$_GET['page'].'.php';
-	} else {
-		include 'pages/notfound.php';
-	}
+    $pages  = preg_replace('(pages/|.php)', "", glob('pages/*.php'));
+    $page  = htmlspecialchars($_GET['page'], ENT_QUOTES);
+    
+	include 'pages/'. (in_array($page, $pages) ? $page : 'notfound') .'.php';
 } else {
-	if ($settings->premium){
-		include 'pages/home.php';
-	} else {
-		include 'pages/servers.php';
-
-	}
-	
+    include 'pages/home.php'; 
 }
 
 include 'template/overall_footer.php';
